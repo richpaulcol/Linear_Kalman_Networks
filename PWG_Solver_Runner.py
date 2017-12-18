9eb4731a4b7bb7cdfbabb7243f4e79e02e5ab0dc
Richard Collins <dickie@sheffield.ac.uk>
2017-12-18 20:14:42 +0000

    Initial Files from old solver

---
+++ PWG_Solver_Runner.py
@@ -0,0 +1,32 @@
+import pylab as pp
+import numpy as np
+from PWG_EPA_tools import *
+
+#FileName = 'Trials/EPANet_Transient_Case_2.inp'
+#FileName = 'Trials/Net3.inp'
+#FileName = 'Trials/1Pipe.inp'
+FileName = 'Trials/hanoi2.inp'
+Net = Import_EPANet_Geom(FileName)
+#Net.geom_Plot(plot_Node_Names = True)
+Net.Import_EPANet_Results()
+Net.Constant_Wavespeed(500)
+Net.MOC_Initialisation(0.1)
+Net.Control_Input('Trials/StochasticDemandsPWGInput.csv')
+Net.MOC_Run(1000)
+#Net.MOC_Run(86350)
+#Net.geom_Plot(plot_Node_Names = True)
+Net.transient_Node_Plot(['6','10','13','16','21','24','31'])
+
+
+#for Node in Net.nodes:
+#	np.save(Node.Name,Node.TranH)
+	
+#for Node in Net.nodes:
+#	pp.scatter([int(Node.Name)], [np.mean(Node.TranH)])
+PE = np.zeros(9999)#999)
+KE = np.zeros(9999)
+for pipe in Net.pipes:
+	PE += np.array(pipe.PE)
+	KE += np.array(pipe.KE)
+
+
