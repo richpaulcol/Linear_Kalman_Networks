e764c735aab9e5bc699c621be97d11ed9d223e81
dickie <r.p.collins@sheffiel.ac.uk>
2018-12-06 13:35:24 +0000

    Old versions

---
+++ Rigid_Column.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pylab as pp
+import wntr as wntr
+
+
+Directory = 'Projects/Real_Networks/'
+FileName = 'ExampleNetwork1.inp'
+
+
+wn = wntr.network.WaterNetworkModel(Directory+FileName)
+
+
+
